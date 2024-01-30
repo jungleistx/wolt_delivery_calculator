f91dd2f532894f34e4fef1490b1bafb5b7915359
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-01-30 19:14:42 +0200

    mod validation.py, add validate_cart_field_types()

--- src/validation.py
+++ src/validation.py
@@ -17,11 +17,21 @@ def validate_cart_keys(cart_keys:dict) -> bool:
 	return True
 
 
+def validate_cart_field_types(cart):
+	validations = [
+		isinstance(cart['cart_value'], int),
+		isinstance(cart['delivery_distance'], int),
+		isinstance(cart['number_of_items'], int),
+		isinstance(cart['time'], str)
+	]
+	return all(validations)
+
+
 def validate_cart_details(cart_details:dict) -> bool:
 	if not cart_details:
 		return {"error": "Missing input, please provide the required fields!"}
 	if not validate_cart_keys(cart_details):
 		return {'error': 'Invalid input, field(s) missing!'}
-	if not validate_cart_content(cart_details):
-		return {'error': 'Invalid input!'}
+	if not validate_cart_field_types(cart_details):
+		return {'error': 'Invalid input, wrong field-type(s)!'}
 	return None
