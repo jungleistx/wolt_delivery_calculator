8d87a5422fe3d61c8f9df67916a977f5a678b70d
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-01-31 16:15:53 +0200

    add tests, clean tests

--- tests/test_invalid_input.py
+++ tests/test_invalid_input.py
@@ -2,10 +2,10 @@ import json
 
 
 DEFAULT_DATA = {
-	"cart_value": 1100,
-	"delivery_distance": 900,
-	"number_of_items": 1,
-	"time": "2024-01-15T13:00:00Z"
+	'cart_value': 1100,
+	'delivery_distance': 900,
+	'number_of_items': 1,
+	'time': '2024-01-15T13:00:00Z'
 }
 
 
@@ -34,7 +34,7 @@ def test_missing_field(client):
 
 def test_wrong_field_type(client):
 	data = DEFAULT_DATA.copy()
-	data['cart_value'] = "150"
+	data['cart_value'] = '150'
 
 	response = client.post('/', json=data)
 	result = json.loads(response.data)
@@ -73,7 +73,7 @@ def test_wrong_method(client):
 
 def test_invalid_time(client):
 	data = DEFAULT_DATA.copy()
-	data['time'] = "2024-01-35T13:00:00Z"
+	data['time'] = '2024-01-35T13:00:00Z'
 
 	response = client.post('/', json=data)
 	result = json.loads(response.data)
@@ -96,6 +96,14 @@ def test_wrong_content_type(client):
 	headers = {'Content-Type': 'text/html'}
 
 	response = client.post('/', json=data, headers=headers)
+	result = json.loads(response.data)
+
+	assert response.status_code == 415
+	assert 'error' in result
+
+
+def test_no_json(client):
+	response = client.post('/')
 	result = json.loads(response.data)
 
 	assert response.status_code == 415
