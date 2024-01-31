0cee26681bd6b4a41218000c2daadc13536d7dee
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2024-01-31 16:18:01 +0200

    clean tests

--- tests/test_validation_functions.py
+++ tests/test_validation_functions.py
@@ -2,10 +2,10 @@ from src.validation import *
 
 
 DEFAULT_DATA = {
-	"cart_value": 1100,
-	"delivery_distance": 900,
-	"number_of_items": 1,
-	"time": "2024-01-15T13:00:00Z"
+	'cart_value': 1100,
+	'delivery_distance': 900,
+	'number_of_items': 1,
+	'time': '2024-01-15T13:00:00Z'
 }
 
 
@@ -22,13 +22,13 @@ def test_validate_cart_keys(client):
 	assert validate_cart_keys(data) == False
 
 	data['delivery_distance'] = 50
-	del data["number_of_items"]
+	del data['number_of_items']
 	assert validate_cart_keys(data) == False
 
-	data["number_of_items"] = 1
-	del data["time"]
+	data['number_of_items'] = 1
+	del data['time']
 	assert validate_cart_keys(data) == False
-	data["time"] = "2024-01-15T13:20:00Z"
+	data['time'] = '2024-01-15T13:20:00Z'
 	assert validate_cart_keys(data) == True
 
 
@@ -36,17 +36,17 @@ def test_validate_cart_field_types(client):
 	data = DEFAULT_DATA.copy()
 	assert validate_cart_field_types(data) == True
 
-	data["cart_value"] = ""
+	data['cart_value'] = ''
 	assert validate_cart_field_types(data) == False
 
-	data["cart_value"] = 50
+	data['cart_value'] = 50
 	assert validate_cart_field_types(data) == True
 
-	data["delivery_distance"] = ""
+	data['delivery_distance'] = ''
 	assert validate_cart_field_types(data) == False
 
-	data["delivery_distance"] = 100
-	data["time"] = 50
+	data['delivery_distance'] = 100
+	data['time'] = 50
 	assert validate_cart_field_types(data) == False
 
 
@@ -84,8 +84,8 @@ def test_validate_cart_time():
 	data = DEFAULT_DATA.copy()
 	assert validate_cart_time(data['time']) == True
 
-	data['time'] = "2024-01-15T29:00:00Z"
+	data['time'] = '2024-01-15T29:00:00Z'
 	assert validate_cart_time(data['time']) == False
 
-	data['time'] = "2050-01-35T19:00:00Z"
+	data['time'] = '2050-01-35T19:00:00Z'
 	assert validate_cart_time(data['time']) == False
